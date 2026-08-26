70b9a36682a7599e82dc054854bcd4e914fdc8f0
Duda-rmello0 <dduda.rmello@gmail.com>
2026-08-26 09:07:49 -0300

    Update

--- modulo05/modulo05_desafio_funcoes_python_mariaeduardamello.py
+++ modulo05/modulo05_desafio_funcoes_python_mariaeduardamello.py
@@ -2,20 +2,18 @@ from faker import Faker
 
 fake = Faker('pt_BR')
 
-usuarios_cadastrados = {
-    fake.user_name(): fake.password(length=8)
-    for _ in range(3)
-}
-usuarios_cadastrados["aluno123"] = "senha123"
+usuarios_cadastrados = {}
+for _ in range(3):
+    usuarios_cadastrados[fake.user_name()] = fake.password(length=8)
 
 def validar_login(usuario, senha, base_dados):
   if usuario in base_dados and base_dados[usuario] == senha:
     return True
   return False
 
-print("--- Usuários e Senhas Gerados pelo Faker ---")
+print("--- Usuários e Senhas Gerados ---")
 print(usuarios_cadastrados)
-print("-" * 43)
+print("-" * 33)
 
 usuario_input = input("Digite o usuário: ")
 senha_input = input("Digite a senha: ")
